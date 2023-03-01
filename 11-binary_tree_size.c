6ef6493401fc008aa4271314ccec6483bd1d2f16
AbdurrahmanIdr <abdurrahmaneedrees@gmail.com>
2023-03-01 12:12:51 +0100

    11-binary_tree_size.c

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,20 +1,24 @@
+#include <stdlib.h>
 #include "binary_trees.h"
 
 /**
- * binary_tree_size - measures the height of a binary tree
- * @tree:  pointer to the root node of the tree to measure the height
- * Return: If tree is NULL, your function must return 0
+ * binary_tree_size - counts data stored from a node as size
+ * @tree: pointer to node
+ *
+ * Return: size of tree
  */
+
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-	size_t left = 0;
-	size_t right = 0;
+	const binary_tree_t *ptr = tree;
+	size_t size = 1;
 
-	if (tree == NULL)
+	if (ptr == NULL)
 		return (0);
 
-	left = binary_tree_size(tree->left);
-	right = binary_tree_size(tree->right);
-
-	return (left + right + 1);
+	if (ptr->left)
+		size += binary_tree_size(ptr->left);
+	if (ptr->right)
+		size += binary_tree_size(ptr->right);
+	return (size);
 }

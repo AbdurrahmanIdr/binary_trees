78e3f14807d14a31521908fd903ac0cc6f304776
AbdurrahmanIdr <abdurrahmaneedrees@gmail.com>
2023-03-01 12:13:57 +0100

    6-binary_tree_preorder.c

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -1,17 +1,24 @@
+#include <stdlib.h>
 #include "binary_trees.h"
 
 /**
- * binary_tree_preorder - function that creates a binary tree node
- * @tree: pointer to the parent node of the node to create
- * @func: pointer of function
- * Return: pointer to the new node, or NULL on failure
+ * binary_tree_preorder - transversal in preorder
+ * @tree: pointer to the node of tree
+ * @func: pointer to print functio
+ *
+ * Return: nothing just print
  */
+
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (tree == NULL || func == NULL)
-		return;
+	const binary_tree_t *ptr = tree;
 
-	func(tree->n);
-	binary_tree_preorder(tree->left, func);
-	binary_tree_preorder(tree->right, func);
+	if (ptr == NULL || func == NULL)
+		return;
+	if (ptr != NULL)
+		func(ptr->n);
+	if (ptr->left != NULL)
+		binary_tree_preorder(ptr->left, func);
+	if (ptr->right != NULL)
+		binary_tree_preorder(ptr->right, func);
 }
